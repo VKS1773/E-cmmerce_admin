c5430b25f476850e9b1b0a7859efc427c2a4b6ae
Vikash Sharma <vs8824394430@gmail.com>
2024-02-14 23:11:04 +0530

    create complte admin panel

---
+++ src/Components/Listproduct/Listproduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Listproduct.css'
+const Listproduct = () => {
+  return (
+    <div className="list-product">
+      
+    </div>
+  )
+}
+
+export default Listproduct
